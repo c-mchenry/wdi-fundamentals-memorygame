d31576698a1e5ba59de7837e975f2c9220a10f6a
Colin McHenry <mchenrycolin@gmail.com>
2018-08-03 14:17:27 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
+var cards = ["queen", "queen", "king", "king"]
+var cardOne = cards[0]
+var cardTwo = cards[2]
+var cardsInPlay = []
+
+cardsInPlay.push(cards[0])
+cardsInPlay.push(cards[2])
+
+
 console.log("Up and running!");
-var cardOne = "queen"
-var cardTwo = "queen"
-var cardThree = "king"
-var cardFour = "king"
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2){
+} if (cardsInPlay[0] === cardsInPlay[2]){
+	alert("You found a match!");
+} else (cardsInPlay[0] !== cardsInPlay[2])
+	alert("Sorry, try again.");
+
